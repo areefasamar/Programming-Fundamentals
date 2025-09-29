41534c7254236aa94d90b14c59b88bf8ffaaf522
Areefa Samar <samarareefa@gmail.com>
2025-09-29 22:51:14 +0500

    Add files via upload

---
+++ PF Lab 02/Question04.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void)
+{  
+  printf("   *********\t\t   ***   \t\t  *  \t\t    *    \n   *\t   *\t\t *     *\t\t ***\t\t  *   *\n   *\t   *\t\t*       *\t\t*****\t\t *     *\n   *\t   *\t\t*       *\t\t  *  \t\t*       *\n   *\t   *\t\t*       *\t\t  *  \t       *\t *\n   *\t   *\t\t*       *\t\t  *  \t\t*       *\n   *\t   *\t\t*       *\t\t  *  \t\t *     *\n   *\t   *\t\t *     *\t\t  *  \t\t  *   *\n   *********\t\t   ***   \t\t  *  \t\t    *    \n"); 
+ return 0;
+}
