1a888e51cc3ca2b5672f418ae26506f70fafb262
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2025-11-20 08:38:23 +0500

    Add files via upload

---
+++ Python Lab/Lab 01/Question02.py
@@ -0,0 +1,4 @@
+# Program to return the length of a list
+array = list(map(int, input("Enter numbers separated by space: ").split()))
+length = len(array)
+print(length)
---
+++ Python Lab/Lab 01/Question06.py
@@ -0,0 +1,7 @@
+# Finding fruit at the index 
+
+fruits = ["Apple", "Banana", "Mangoes", "Orange", "Grapes"]
+
+index = int(input("Enter an index (0-4): "))
+
+print("Fruit at index", index, "is:", fruits[index])
