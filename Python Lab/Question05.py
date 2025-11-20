4bfc5d6f8e5e61f4d3badaddc744d92e35a19103
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2025-11-20 08:32:31 +0500

    Delete Python Lab directory

--- Python Lab/Question05.py
+++
@@ -1,7 +0,0 @@
-# Find the average of three numbers
-a = float(input())
-b = float(input())
-c = float(input())
-
-average = (a + b + c) / 3
-print("Average =", average)
--- Python Lab/Question06.py
+++
@@ -1,7 +0,0 @@
-# Finding fruit at the index 
-
-fruits = ["Apple", "Banana", "Mangoes", "Orange", "Grapes"]
-
-index = int(input("Enter an index (0-4): "))
-
-print("Fruit at index", index, "is:", fruits[index])
